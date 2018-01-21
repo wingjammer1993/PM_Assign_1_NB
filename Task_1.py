58b7aa485e44dbe8c7f075ae937db3918b4a719b
Amruta <hail.radical@gmail.com>
2018-01-21 16:51:51 -0700

    task_0

--- Task_1.py
+++ Task_1.py
@@ -9,13 +9,17 @@ def generate_conditional(joint_probabilities):
     for i in table:
         tup_1 = (i[0], i[1], i[2], 'no')
         tup_2 = (i[0], i[1], i[2], 'yes')
+        a = 0
+        b = 0
 
         if tup_1 in joint_probabilities:
             a = joint_probabilities[tup_1]
-            if tup_2 in joint_probabilities:
-                b = joint_probabilities[tup_2]
-                denominator = a + b
-                data_conditional_probabilities[i] = a / denominator
+        if tup_2 in joint_probabilities:
+            b = joint_probabilities[tup_2]
+
+        if a > 0 or b > 0:
+            denominator = a + b
+            data_conditional_probabilities[i] = a / denominator
         else:
             data_conditional_probabilities[i] = 'data not available'
     return data_conditional_probabilities
